9a5d0c694126c8ebcf2c327426ae5dd65e2c8b4a
autotaker <autotaker@gmail.com>
2020-01-29 01:04:53 +0900

    add value definition

---
+++ src/Smopeck/Mock/Value.hs
@@ -0,0 +1,18 @@
+module Smopeck.Mock.Value where
+
+import qualified Data.Map              as M
+import           Data.Scientific
+import qualified Data.Set              as S
+import           Smopeck.Mock.Location
+import           Smopeck.Spec.Syntax
+
+data Value =
+    VNull
+    | VBool Bool
+    | VNumber Scientific
+    | VString String
+    | VObject (S.Set FieldName)
+    | VArray
+    deriving(Show)
+
+type Assignment = M.Map Location Value
