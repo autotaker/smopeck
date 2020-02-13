d33925bfd3335009cd421bdbc7d32cd032e5a0cb
autotaker <autotaker@gmail.com>
2020-02-14 08:25:29 +0900

    implement range model

--- src/Smopeck/Logic/Number.hs
+++ src/Smopeck/Logic/Number.hs
@@ -2,6 +2,7 @@
 module Smopeck.Logic.Number where
 
 import           Control.Monad.Primitive
+import           Data.Coerce
 import           Data.Scientific
 import           System.Random.MWC
 
@@ -14,92 +15,108 @@ class Model m where
     generate :: PrimMonad f => Gen (PrimState f) -> Atom m -> f (Maybe m)
 
 data End a = Open | Inclusive a | Exclusive a
-data Range a = Range { leftEnd :: End a,  rightEnd :: End a }
+    deriving(Eq,Ord,Show)
+data Range a = Range { leftEnd :: LeftEnd a,  rightEnd :: RightEnd a }
+    deriving(Eq,Ord,Show)
 type RangeArea a = [Range a]
 
-instance Model Scientific where
-    newtype Atom Scientific = RangeArea [Range Scientific]
-    top = RangeArea [Range Open Open]
-    bot = RangeArea []
-    join (RangeArea x) (RangeArea y) = RangeArea (joinArea x y)
-    meet = undefined
-    generate = undefined
+newtype LeftEnd a = LeftEnd (End a) deriving (Eq, Show)
+newtype RightEnd a = RightEnd (End a) deriving (Eq, Show)
+
+instance Ord a => Ord (LeftEnd a) where
+    compare (LeftEnd a) (LeftEnd b) =
+        case (a, b) of
+            (Open, Open)               -> EQ
+            (Open, _)                  -> LT
+            (_, Open)                  -> GT
+            (Inclusive a, Inclusive b) -> compare a b
+            (Inclusive a, Exclusive b) -> if a <= b then LT else GT
+            (Exclusive a, Inclusive b) -> if a < b then LT else GT
+            (Exclusive a, Exclusive b) -> compare a b
+
+instance Ord a => Ord (RightEnd a) where
+    compare (RightEnd a) (RightEnd b) =
+        case (a, b) of
+            (Open, Open)               -> EQ
+            (Open, _)                  -> GT
+            (_, Open)                  -> LT
+            (Inclusive a, Inclusive b) -> compare a b
+            (Inclusive a, Exclusive b) -> if a >= b then GT else LT
+            (Exclusive a, Inclusive b) -> if a > b then GT else LT
+            (Exclusive a, Exclusive b) -> compare a b
+
+instance Model Double where
+    newtype Atom Double = RangeAreaD [Range Double]
+    top = RangeAreaD [Range (LeftEnd Open) (RightEnd Open)]
+    bot = RangeAreaD []
+    join x y = RangeAreaD (joinArea (coerce x) (coerce y))
+    meet x y = RangeAreaD (meetArea (coerce x) (coerce y))
+    generate st xs = generateArea st eps (coerce xs)
+        where eps x = x * 1e-53
+
+instance Model Int where
+    newtype Atom Int = RangeAreaI [Range Int]
+    top = RangeAreaI [Range (LeftEnd Open) (RightEnd Open)]
+    bot = RangeAreaI []
+    join x y = RangeAreaI (joinArea (coerce x) (coerce y))
+    meet x y = RangeAreaI (meetArea (coerce x) (coerce y))
+    generate st xs = generateArea st eps (coerce xs)
+        where eps x = 1
 
 
--- return true if rightEnd intersect with leftEnd
-intersectEnd :: Ord a => End a -> End a -> Bool
-intersectEnd Open _                      = True
-intersectEnd _ Open                      = True
-intersectEnd (Inclusive a) (Inclusive b) = a >= b
-intersectEnd (Inclusive a) (Exclusive b) = a > b
-intersectEnd (Exclusive a) (Inclusive b) = a > b
-intersectEnd (Exclusive a) (Exclusive b) = a > b
+nonEmpty :: Ord a => LeftEnd a -> RightEnd a -> Bool
+nonEmpty (LeftEnd b) (RightEnd a) =
+    case (a, b) of
+        (Open, _)                  -> True
+        (_, Open)                  -> True
+        (Inclusive a, Inclusive b) -> a >= b
+        (Inclusive a, Exclusive b) -> a > b
+        (Exclusive a, Inclusive b) -> a > b
+        (Exclusive a, Exclusive b) -> a > b
 
 intersect :: Ord a => Range a -> Range a -> Bool
-intersect (Range l1 r1) (Range l2 r2) = intersectEnd r1 l2 && intersectEnd r2 l1
+intersect (Range l1 r1) (Range l2 r2) = nonEmpty l2 r1 && nonEmpty l1 r2
 
 joinRange :: Ord a => Range a -> Range a -> Range a
-joinRange (Range l1 r1) (Range l2 r2) = Range (minEnd l1 l2) (maxEnd r1 r2)
-    where
-    minEnd Open _ = Open
-    minEnd _ Open = Open
-    minEnd (Inclusive a) (Inclusive b) = Inclusive (min a b)
-    minEnd (Exclusive a) (Exclusive b) = Exclusive (min a b)
-    minEnd (Inclusive a) (Exclusive b)
-        | a <= b = Inclusive a
-        | otherwise = Exclusive b
-    minEnd (Exclusive b) (Inclusive a)
-        | a <= b = Inclusive a
-        | otherwise = Exclusive b
-    maxEnd Open _ = Open
-    maxEnd _ Open = Open
-    maxEnd (Inclusive a) (Inclusive b) = Inclusive (max a b)
-    maxEnd (Exclusive a) (Exclusive b) = Exclusive (max a b)
-    maxEnd (Inclusive a) (Exclusive b)
-        | a >= b = Inclusive a
-        | otherwise = Exclusive b
-    maxEnd (Exclusive b) (Inclusive a)
-        | a >= b = Inclusive a
-        | otherwise = Exclusive b
+joinRange (Range l1 r1) (Range l2 r2) = Range (min l1 l2) (max r1 r2)
 
 meetRange :: Ord a => Range a -> Range a -> Range a
-meetRange (Range l1 r1) (Range l2 r2) = Range (minEnd l1 l2) (maxEnd r1 r2)
-    where
-    minEnd Open y = y
-    minEnd x Open = x
-    minEnd (Inclusive a) (Inclusive b) = Inclusive (max a b)
-    minEnd (Exclusive a) (Exclusive b) = Exclusive (max a b)
-    minEnd (Inclusive a) (Exclusive b)
-        | a <= b = Exclusive b
-        | otherwise = Inclusive a
-    minEnd (Exclusive b) (Inclusive a)
-        | a <= b = Exclusive b
-        | otherwise = Inclusive a
-    maxEnd Open y = y
-    maxEnd x Open = x
-    maxEnd (Inclusive a) (Inclusive b) = Inclusive (min a b)
-    maxEnd (Exclusive a) (Exclusive b) = Exclusive (min a b)
-    maxEnd (Inclusive a) (Exclusive b)
-        | a >= b = Exclusive b
-        | otherwise = Inclusive a
-    maxEnd (Exclusive b) (Inclusive a)
-        | a >= b = Exclusive b
-        | otherwise = Inclusive a
-
+meetRange (Range l1 r1) (Range l2 r2) = Range (max l1 l2) (min r1 r2)
 
 joinArea :: Ord a => RangeArea a -> RangeArea a -> RangeArea a
 joinArea [] ys = ys
 joinArea xs [] = xs
 joinArea (x:xs) (y:ys)
     | intersect x y = joinArea (joinRange x y:xs) ys
-    | leftEnd x `isLeftOf` leftEnd y = x : joinArea xs (y:ys)
+    | leftEnd x <= leftEnd y = x : joinArea xs (y:ys)
     | otherwise = y : joinArea (x:xs) ys
 
+meetArea :: Ord a => RangeArea a -> RangeArea a -> RangeArea a
+meetArea [] _ = []
+meetArea _ [] = []
+meetArea (x:xs) (y:ys)
+    | intersect x y = meetRange x y : meetArea xs' ys'
+    | otherwise = meetArea xs' ys'
+    where
+        (xs', ys')
+            | rightEnd x <= rightEnd y = (xs, y:ys)
+            | otherwise = (x:xs, ys)
 
-isLeftOf :: Ord a => End a -> End a -> Bool
-isLeftOf Open _                      = True
-isLeftOf _ Open                      = False
-isLeftOf (Inclusive a) (Inclusive b) = a <= b
-isLeftOf (Inclusive a) (Exclusive b) = a <= b
-isLeftOf (Exclusive a) (Inclusive b) = a < b
-isLeftOf (Exclusive a) (Exclusive b) = a <= b
+generateArea :: (Variate a, Num a, PrimMonad f) => Gen (PrimState f) -> (a -> a) -> RangeArea a -> f (Maybe a)
+generateArea st eps [] = pure Nothing
+generateArea st eps xs = do
+    let n = length xs
+    x <- (xs !!) <$> uniformR (0, n-1) st
+    let leftBound = case leftEnd x of
+            LeftEnd Open          -> Nothing
+            LeftEnd (Inclusive a) -> pure a
+            LeftEnd (Exclusive a) -> pure $ a + eps a
+        rightBound = case rightEnd x of
+            RightEnd Open          -> Nothing
+            RightEnd (Inclusive a) -> pure a
+            RightEnd (Exclusive a) -> pure $ a - eps a
+    case (leftBound, rightBound) of
+        (Nothing, Nothing) -> Just <$> uniformR (-100, 100) st
+        (Just a, Nothing)  -> Just <$> uniformR (a , a + 100) st
+        (Nothing, Just a)  -> Just <$> uniformR (a - 100, a) st
+        (Just a, Just b)   -> Just <$> uniformR (a, b) st
