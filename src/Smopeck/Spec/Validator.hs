9ae6a82ab16e0c29dda34e37883eb91d74632b45
autotaker <autotaker@gmail.com>
2020-03-28 13:39:52 +0900

    implement validator

--- src/Smopeck/Spec/Validator.hs
+++ src/Smopeck/Spec/Validator.hs
@@ -4,15 +4,41 @@ module Smopeck.Spec.Validator where
 
 import           Control.Applicative
 import           Control.Monad.Except
+import qualified Data.Aeson            as A
+import qualified Data.HashMap.Strict   as HM
 import qualified Data.Map              as M
 import           Data.Scientific
 import qualified Data.Set              as S
+import qualified Data.Text             as T
+import qualified Data.Vector           as V
 import           Smopeck.Mock.Location
 import           Smopeck.Mock.Value
 import           Smopeck.Spec.Exp
 import           Smopeck.Spec.Lattice
 import           Smopeck.Spec.TypeExp
 
+validateJson :: WHNFTypeEnv Desugar -> M.Map String A.Value
+                -> String -> TypeExp Desugar WHNF -> Except String ()
+validateJson tyEnv env x = validate tyEnv env' loc
+    where
+    loc = Root (Absolute x)
+    env' = M.fromList $ M.assocs env >>= unfold
+    unfold :: (String, A.Value) -> [ (ALocation, Value )]
+    unfold (root, val) = go (Root (Absolute root)) val
+    go it v = case v of
+        A.Null -> pure (it, VNull)
+        A.Bool b -> pure (it, VBool b)
+        A.Number n -> pure (it, VNumber n)
+        A.String s -> pure (it, VString (T.unpack s))
+        A.Object obj ->
+            pure (it, VObject fields) <>
+            (HM.toList obj >>= \(key, val) -> go (it `Field` T.unpack key) val)
+            where
+            fields = S.fromList $ map T.unpack $ HM.keys obj
+        A.Array arr -> pure (it, VArray) <>
+            pure (it `Field` "length", VNumber (fromIntegral n)) <>
+            ([0..n-1] >>= (\i -> go (it `Get` i) (arr V.! i)))
+            where n = length arr
 
 validate :: WHNFTypeEnv Desugar -> M.Map ALocation Value -> ALocation -> TypeExp Desugar WHNF -> Except String ()
 validate tyEnv env = goLattice
