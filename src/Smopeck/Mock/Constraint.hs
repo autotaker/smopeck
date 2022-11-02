830dc51b73ab4da9239f7f18d9036496190a4096
autotaker <autotaker@gmail.com>
2022-11-03 02:24:03 +0900

    version up to ghc 9.2.4

--- src/Smopeck/Mock/Constraint.hs
+++ src/Smopeck/Mock/Constraint.hs
@@ -1,129 +1,151 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE GADTs         #-}
-{-# LANGUAGE LambdaCase    #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TupleSections #-}
+
 module Smopeck.Mock.Constraint where
 
-import           Control.Monad.Reader    hiding (join)
-import qualified Data.Aeson              as JSON
-import           Data.Bifunctor
-import           Data.Foldable
-import           Data.Function
-import qualified Data.HashMap.Strict     as HM
-import qualified Data.HashTable.IO       as H
-import qualified Data.Map                as M
-import           Data.Scientific
-import qualified Data.Set                as S
-import qualified Data.Text               as T
-import qualified Data.Vector             as V
-import           Smopeck.Logic.Equality
-import           Smopeck.Logic.Model
-import           Smopeck.Logic.Number
-import           Smopeck.Logic.Regex
-import           Smopeck.Mock.Dependency
-import           Smopeck.Mock.Location
-import           Smopeck.Mock.Value
-import           Smopeck.Spec.Exp        hiding (interpret)
-import qualified Smopeck.Spec.Exp        as Exp
-import           Smopeck.Spec.Lattice
-import qualified Smopeck.Spec.TypeExp    as T
-import           System.Random.MWC
+import Control.Monad.Reader hiding (join)
+import qualified Data.Aeson as JSON
+import qualified Data.Aeson.Key as K
+import qualified Data.Aeson.KeyMap as KM
+import Data.Bifunctor
+import Data.Foldable
+import Data.Function
+import qualified Data.HashMap.Strict as HM
+import qualified Data.HashTable.IO as H
+import qualified Data.Map as M
+import Data.Scientific
+import qualified Data.Set as S
+import Data.String (fromString)
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import Smopeck.Logic.Equality
+import Smopeck.Logic.Model
+import Smopeck.Logic.Number
+import Smopeck.Logic.Regex
+import Smopeck.Mock.Dependency
+import Smopeck.Mock.Location
+import Smopeck.Mock.Value
+import Smopeck.Spec.Exp hiding (interpret)
+import qualified Smopeck.Spec.Exp as Exp
+import Smopeck.Spec.Lattice
+import qualified Smopeck.Spec.TypeExp as T
+import System.Random.MWC
 
 type Exp = T.Exp Desugar
+
 type TypeExp = T.TypeExp Desugar T.WHNF
+
 type TypeExpF = T.TypeExpF Desugar T.WHNF
+
 type TypeEnv = T.WHNFTypeEnv Desugar
+
 type Assertion = Exp
+
 type Predicate = (Op, Exp)
+
 type LocationExp = T.LocationExp Desugar
 
-
 type HashTable k v = H.BasicHashTable k v
+
 type SolveM a = ReaderT Context IO a
 
-data Context = Context {
-  assignment :: HashTable ALocation (Either TypeExp Value),
-  typeEnv    :: TypeEnv,
-  randState  :: GenIO
-}
+data Context = Context
+  { assignment :: HashTable ALocation (Either TypeExp Value),
+    typeEnv :: TypeEnv,
+    randState :: GenIO
+  }
 
 initContext :: TypeEnv -> IO Context
 initContext env = Context <$> H.new <*> pure env <*> createSystemRandom
 
 genInstance :: Model m => Lattice Full (Op, Literal Desugar) -> SolveM m
 genInstance lat = do
-  let g = cata CataFull{
-            fFBot = bot,
-            fFTop = top,
-            fFJoin = join,
-            fFMeet = meet,
-            fFElem = uncurry interpret
-          } lat
+  let g =
+        cata
+          CataFull
+            { fFBot = bot,
+              fFTop = top,
+              fFJoin = join,
+              fFMeet = meet,
+              fFElem = uncurry interpret
+            }
+          lat
   gen <- asks randState
   r <- generate gen g
   case r of
-    Just v  -> pure v
+    Just v -> pure v
     Nothing -> error "no such value"
-
-
 
 chooseShape :: ALocation -> TypeExp -> SolveM TypeExpF
 chooseShape base ty = do
   gen <- asks randState
-  cands <- filterM (\tyf ->
-    case T.typeExpCond tyf of
-      T.HasCond e -> do
-        v <- evalExp base e >>= deref base
-        case v of
-          LBool True -> pure True
-          LBool False -> pure False
-          _ -> error $ "condition " ++ show e ++ " is evaluated to non Boolean: " ++ show v
-      T.NoCond  -> pure True) $ toList ty
+  cands <-
+    filterM
+      ( \tyf ->
+          case T.typeExpCond tyf of
+            T.HasCond e -> do
+              v <- evalExp base e >>= deref base
+              case v of
+                LBool True -> pure True
+                LBool False -> pure False
+                _ -> error $ "condition " ++ show e ++ " is evaluated to non Boolean: " ++ show v
+            T.NoCond -> pure True
+      )
+      $ toList ty
   when (null cands) $ error $ "no candidates: " ++ show (base, ty)
   idx <- uniformR (0, length cands - 1) gen
   pure $ cands !! idx
 
 extract :: ALocation -> SolveM JSON.Value
-extract loc = evalL loc (castRoot loc) >>= \case
-  VBool b -> pure $ JSON.Bool  b
-  VNumber n -> pure $ JSON.Number n
-  VNull -> pure $ JSON.Null
-  VString s -> pure $ JSON.toJSON s
-  VObject fields -> do
-    pairs <- mapM (\f -> do
-      v <- extract (loc `Field` f)
-      pure $ T.pack f JSON..= v) $ S.toList fields
-    pure $ JSON.object pairs
-  VArray -> do
-    JSON.Number len <- extract $ loc `Field` "length"
-    es <- forM [0..floor len - 1] $ extract . (Get loc)
-    pure $ JSON.toJSON es
+extract loc =
+  evalL loc (castRoot loc) >>= \case
+    VBool b -> pure $ JSON.Bool b
+    VNumber n -> pure $ JSON.Number n
+    VNull -> pure JSON.Null
+    VString s -> pure $ JSON.toJSON s
+    VObject fields -> do
+      pairs <-
+        mapM
+          ( \f -> do
+              v <- extract (loc `Field` f)
+              pure $ fromString f JSON..= v
+          )
+          $ S.toList fields
+      pure $ JSON.object pairs
+    VArray -> do
+      JSON.Number len <- extract $ loc `Field` "length"
+      es <- forM [0 .. floor len - 1] $ extract . Get loc
+      pure $ JSON.toJSON es
 
 mockJson :: TypeEnv -> TypeExp -> IO JSON.Value
 mockJson env ty = initContext env >>= mockJsonWithContext env ty
 
 mockJsonWithContext :: TypeEnv -> TypeExp -> Context -> IO JSON.Value
 mockJsonWithContext env ty = runReaderT doit
-    where
-      root = Root (Absolute "")
-      doit = do
-        tbl <- asks assignment
-        liftIO $ H.insert tbl root (Left ty)
-        extract root
+  where
+    root = Root (Absolute "")
+    doit = do
+      tbl <- asks assignment
+      liftIO $ H.insert tbl root (Left ty)
+      extract root
 
 mockJsonWithEnv :: TypeEnv -> M.Map String JSON.Value -> TypeExp -> IO JSON.Value
 mockJsonWithEnv env venv ty = do
   ctx <- initContext env
   let insertValue k v = H.insert (assignment ctx) k (Right v)
       insert key (JSON.Object obj) = do
-        let keys = map T.unpack $ HM.keys obj
+        let keys = map K.toString $ KM.keys obj
         insertValue key $ VObject $ S.fromList keys
-        forM_ (HM.toList obj)
-          $ uncurry $ insert . Field key . T.unpack
+        forM_ (KM.toList obj) $
+          uncurry $
+            insert . Field key . K.toString
       insert key (JSON.Array arr) = do
         let size = V.length arr
-        insertValue (key `Field` "length")
-          $ VNumber $ fromIntegral size
+        insertValue (key `Field` "length") $
+          VNumber $
+            fromIntegral size
         V.imapM_ (insert . Get key) arr
       insert key (JSON.Number n) = insertValue key $ VNumber n
       insert key (JSON.Bool b) = insertValue key $ VBool b
@@ -131,20 +153,16 @@ mockJsonWithEnv env venv ty = do
         insertValue key $ VString (T.unpack s)
       insert key JSON.Null = insertValue key VNull
 
-  forM_ (M.assocs venv)
-   $ uncurry $ insert . Root . Absolute
+  forM_ (M.assocs venv) $
+    uncurry $
+      insert . Root . Absolute
   mockJsonWithContext env ty ctx
-
-
-
-
-
 
 evalL :: ALocation -> Location -> SolveM Value
 evalL base loc' =
   case resolve base loc' of
     Right i -> pure $ VNumber (fromIntegral i)
-    Left loc ->  do
+    Left loc -> do
       tbl <- asks assignment
       liftIO (H.lookup tbl loc) >>= \case
         Just (Right v) -> pure v
@@ -163,31 +181,31 @@ evalT loc ty = do
     T.Prim T.PNumber -> do
       predicates <-
         T.typeExpRef tyF
-          & mapM (\(op, e) -> LElem . (op, ) <$> (evalExp loc e >>= deref loc))
+          & mapM (\(op, e) -> LElem . (op,) <$> (evalExp loc e >>= deref loc))
       v <- genInstance (foldr LMeet LTop predicates)
       pure (VNumber (fromFloatDigits (v :: Double)))
     T.Prim T.PInt -> do
       predicates <-
         T.typeExpRef tyF
-          & mapM (\(op, e) -> LElem . (op, ) <$> (evalExp loc e >>= deref loc))
+          & mapM (\(op, e) -> LElem . (op,) <$> (evalExp loc e >>= deref loc))
       v <- genInstance (foldr LMeet LTop predicates)
-      pure (VNumber (fromIntegral (v::Int)))
+      pure (VNumber (fromIntegral (v :: Int)))
     T.Prim T.PString -> do
       predicates <-
         T.typeExpRef tyF
-          & mapM (\(op, e) -> LElem . (op, ) <$> (evalExp loc e >>= deref loc))
+          & mapM (\(op, e) -> LElem . (op,) <$> (evalExp loc e >>= deref loc))
       v <- genInstance (foldr LMeet LTop predicates)
       pure (VString v)
     T.Prim T.PBool -> do
       predicates <-
         T.typeExpRef tyF
-          & mapM (\(op, e) -> LElem . (op, ) <$> (evalExp loc e >>= deref loc))
+          & mapM (\(op, e) -> LElem . (op,) <$> (evalExp loc e >>= deref loc))
       (Equality v) <- genInstance (foldr LMeet LTop predicates)
       pure (VBool v)
     T.Prim T.PNull -> pure VNull
     T.Prim T.PObject -> do
       let exts = T.typeExpExt tyF
-          fields = S.fromList [ field | FieldString field <- S.toList $ M.keysSet exts ]
+          fields = S.fromList [field | FieldString field <- S.toList $ M.keysSet exts]
       forM_ (M.toList exts) $ \(FieldString field, tyExp) -> do
         env <- asks typeEnv
         let tyExp' = T.evalTypeExp env tyExp
@@ -203,44 +221,44 @@ evalT loc ty = do
       VNumber len <- evalT (loc `Field` "length") lenTy
       tbl <- asks assignment
       liftIO (H.insert tbl lenLoc (Right (VNumber len)))
-      forM_ [0..floor len - 1] $ \i -> do
+      forM_ [0 .. floor len - 1] $ \i -> do
         let getTy = T.evalTypeExp env (exts M.! FieldIndex T.BindDebrujin)
             getLoc = loc `Get` i
         liftIO (H.insert tbl getLoc (Left getTy))
       pure VArray
 
-
 evalNumber :: ALocation -> Exp -> SolveM Scientific
 evalNumber base e = toNumber <$> (evalExp base e >>= deref base)
 
 deref :: ALocation -> Either Location (Literal Desugar) -> SolveM (Literal Desugar)
-deref _ (Right l)   = pure l
+deref _ (Right l) = pure l
 deref base (Left x) = toLiteral <$> evalL base x
 
 evalExp :: ALocation -> Exp -> SolveM (Either Location (Literal Desugar))
 evalExp base (T.Exp e) = go e
   where
-  go (Literal l)   = pure (Right l)
-  go (Var x)       = Left <$> traverse (fmap floor . evalNumber base) x
-  go (App op args) = Right . Exp.interpret op <$> mapM f args
-    where
-      f e = go e >>= \case
-        Right l -> pure l
-        Left x -> toLiteral <$> evalL base x
+    go (Literal l) = pure (Right l)
+    go (Var x) = Left <$> traverse (fmap floor . evalNumber base) x
+    go (App op args) = Right . Exp.interpret op <$> mapM f args
+      where
+        f e =
+          go e >>= \case
+            Right l -> pure l
+            Left x -> toLiteral <$> evalL base x
 
 toLiteral :: Value -> Literal Desugar
-toLiteral (VBool b)   = LBool b
+toLiteral (VBool b) = LBool b
 toLiteral (VNumber n) = LNumber n
-toLiteral VNull       = LNull
+toLiteral VNull = LNull
 toLiteral (VString s) = LString s
 toLiteral (VObject _) = error "cannot convert object to literal"
-toLiteral VArray      = error "cannot convert array to literal"
+toLiteral VArray = error "cannot convert array to literal"
 
 toNumber :: Literal m -> Scientific
 toNumber (LNumber n) = n
-toNumber _           = error "cannot convert to number"
+toNumber _ = error "cannot convert to number"
 
 parent :: LocationF (Root blob) Int -> LocationF (Root blob) Int
-parent (p `Get` _)   = p
+parent (p `Get` _) = p
 parent (p `Field` _) = p
-parent (Root _)      = error "no parent for root"
+parent (Root _) = error "no parent for root"
--- src/Smopeck/Spec/Validator.hs
+++ src/Smopeck/Spec/Validator.hs
@@ -1,157 +1,174 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE GADTs           #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE RecordWildCards #-}
+
 module Smopeck.Spec.Validator where
 
-import           Control.Applicative
-import           Control.Monad.Except
-import qualified Data.Aeson            as A
-import           Data.Char
-import qualified Data.HashMap.Strict   as HM
-import qualified Data.Map              as M
-import           Data.Scientific
-import qualified Data.Set              as S
-import qualified Data.Text             as T
-import qualified Data.Vector           as V
-import           Smopeck.Mock.Location
-import           Smopeck.Mock.Value
-import           Smopeck.Spec.Exp
-import           Smopeck.Spec.Lattice
-import           Smopeck.Spec.TypeExp
-import           Text.Read
+import Control.Applicative
+import Control.Monad.Except
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Key as K
+import qualified Data.Aeson.KeyMap as KM
+import Data.Char
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Map as M
+import Data.Scientific
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import Smopeck.Mock.Location
+import Smopeck.Mock.Value
+import Smopeck.Spec.Exp
+import Smopeck.Spec.Lattice
+import Smopeck.Spec.TypeExp
+import Text.Read
 
-
-validateJson :: WHNFTypeEnv Desugar -> M.Map String A.Value
-                -> String -> TypeExp Desugar WHNF -> Either String ()
+validateJson ::
+  WHNFTypeEnv Desugar ->
+  M.Map String A.Value ->
+  String ->
+  TypeExp Desugar WHNF ->
+  Either String ()
 validateJson tyEnv env x = runExcept . validate tyEnv env' loc
-    where
+  where
     loc = Root (Absolute x)
     env' = M.fromList $ M.assocs env >>= unfold
-    unfold :: (String, A.Value) -> [ (ALocation, Value )]
+    unfold :: (String, A.Value) -> [(ALocation, Value)]
     unfold (root, val) = go (Root (Absolute root)) val
     go it v = case v of
-        A.Null -> pure (it, VNull)
-        A.Bool b -> pure (it, VBool b)
-        A.Number n -> pure (it, VNumber n)
-        A.String s -> pure (it, VString (T.unpack s))
-        A.Object obj ->
-            pure (it, VObject fields) <>
-            (HM.toList obj >>= \(key, val) -> go (it `Field` T.unpack key) val)
-            where
-            fields = S.fromList $ map T.unpack $ HM.keys obj
-        A.Array arr -> pure (it, VArray) <>
-            pure (it `Field` "length", VNumber (fromIntegral n)) <>
-            ([0..n-1] >>= (\i -> go (it `Get` i) (arr V.! i)))
-            where n = length arr
+      A.Null -> pure (it, VNull)
+      A.Bool b -> pure (it, VBool b)
+      A.Number n -> pure (it, VNumber n)
+      A.String s -> pure (it, VString (T.unpack s))
+      A.Object obj ->
+        pure (it, VObject fields)
+          <> (KM.toList obj >>= \(key, val) -> go (it `Field` K.toString key) val)
+        where
+          fields = S.fromList $ map K.toString $ KM.keys obj
+      A.Array arr ->
+        pure (it, VArray)
+          <> pure (it `Field` "length", VNumber (fromIntegral n))
+          <> ([0 .. n - 1] >>= (\i -> go (it `Get` i) (arr V.! i)))
+        where
+          n = length arr
 
 validate :: WHNFTypeEnv Desugar -> M.Map ALocation Value -> ALocation -> TypeExp Desugar WHNF -> Except String ()
 validate tyEnv env = goLattice
-    where
+  where
     goLattice :: ALocation -> TypeExp Desugar WHNF -> Except String ()
-    goLattice it = cata CataJoin{
-        fJBot = throwError "bottom type",
-        fJJoin = (<|>),
-        fJElem = go it
-    }
+    goLattice it =
+      cata
+        CataJoin
+          { fJBot = throwError "bottom type",
+            fJJoin = (<|>),
+            fJElem = go it
+          }
     go :: ALocation -> TypeExpF Desugar WHNF -> Except String ()
-    go it TypeExpF{..} = do
-        let value = env M.! it
-            checkCond (HasCond e) = evalExp env it e == LBool True
-            checkCond NoCond      = True
-        unless (checkCond typeExpCond) $ throwError "type cond does not hold"
-        case (typeExpName, value) of
-            (Prim PNull, VNull)       -> pure ()
-            (Prim PNumber, VNumber n) -> goRefs it (LNumber n) typeExpRef
-            (Prim PInt, VNumber n)    -> goRefs it (LNumber n) typeExpRef
-            (Prim PString, VString s) -> goRefs it (LString s) typeExpRef
-            (Prim PBool, VBool b)     -> goRefs it (LBool b) typeExpRef
-            (Prim PObject, VObject fields) ->
-                forM_ (M.assocs typeExpExt) $ \(FieldString field, ty) -> do
-                    unless (field `S.member` fields) $
-                        throwError $ "field " ++ show field ++ " is not member of " ++ show it
-                    goLattice (it `Field` field) (evalTypeExp tyEnv ty)
-            (Prim PArray, VArray) -> do
-                let tyLen = typeExpExt M.! FieldString "length"
-                let VNumber l = env M.! (it `Field` "length")
-                goLattice (it `Field` "length") (evalTypeExp tyEnv tyLen)
-                forM_ [0..floor l - 1] $ \i -> do
-                    let tyElt = typeExpExt M.! FieldIndex BindDebrujin
-                    goLattice (it `Get` i) (evalTypeExp tyEnv tyElt)
-            (Prim ty, _) -> throwError $ "value " ++ show value ++ " is not " ++ show ty
+    go it TypeExpF {..} = do
+      let value = env M.! it
+          checkCond (HasCond e) = evalExp env it e == LBool True
+          checkCond NoCond = True
+      unless (checkCond typeExpCond) $ throwError "type cond does not hold"
+      case (typeExpName, value) of
+        (Prim PNull, VNull) -> pure ()
+        (Prim PNumber, VNumber n) -> goRefs it (LNumber n) typeExpRef
+        (Prim PInt, VNumber n) -> goRefs it (LNumber n) typeExpRef
+        (Prim PString, VString s) -> goRefs it (LString s) typeExpRef
+        (Prim PBool, VBool b) -> goRefs it (LBool b) typeExpRef
+        (Prim PObject, VObject fields) ->
+          forM_ (M.assocs typeExpExt) $ \(FieldString field, ty) -> do
+            unless (field `S.member` fields) $
+              throwError $
+                "field " ++ show field ++ " is not member of " ++ show it
+            goLattice (it `Field` field) (evalTypeExp tyEnv ty)
+        (Prim PArray, VArray) -> do
+          let tyLen = typeExpExt M.! FieldString "length"
+          let VNumber l = env M.! (it `Field` "length")
+          goLattice (it `Field` "length") (evalTypeExp tyEnv tyLen)
+          forM_ [0 .. floor l - 1] $ \i -> do
+            let tyElt = typeExpExt M.! FieldIndex BindDebrujin
+            goLattice (it `Get` i) (evalTypeExp tyEnv tyElt)
+        (Prim ty, _) -> throwError $ "value " ++ show value ++ " is not " ++ show ty
 
     goRefs :: ALocation -> Literal Desugar -> TypeRefine Desugar -> Except String ()
     goRefs it lhs = mapM_ $ \(op, e) ->
-        let rhs = evalExp env it e in
-        unless (interpret op [lhs, rhs] == LBool True) $
+      let rhs = evalExp env it e
+       in unless (interpret op [lhs, rhs] == LBool True) $
             throwError $
-                "expected " ++ show it ++ show op ++ show e ++ "\n"
-                ++ "but found" ++ show lhs ++ show op ++ show rhs
-
+              "expected "
+                ++ show it
+                ++ show op
+                ++ show e
+                ++ "\n"
+                ++ "but found"
+                ++ show lhs
+                ++ show op
+                ++ show rhs
 
 parseParam :: String -> TypeExp Desugar WHNF -> Except String A.Value
-parseParam val = cata CataJoin {
-        fJBot = throwError "void type",
+parseParam val =
+  cata
+    CataJoin
+      { fJBot = throwError "void type",
         fJElem = go,
         fJJoin = (<|>)
-    }
-    where
+      }
+  where
     go :: TypeExpF Desugar WHNF -> Except String A.Value
-    go TypeExpF{..} =
-        case typeExpName of
-            Prim PInt -> do
-                n <- ExceptT $ pure $ fmap fromInteger $ readEither val
-                goRefs (LNumber n) typeExpRef
-                pure $ A.Number n
-            Prim PNumber -> do
-                n <- ExceptT $ pure $ readEither val
-                goRefs (LNumber n) typeExpRef
-                pure $ A.Number n
-            Prim PString -> do
-                goRefs (LString val) typeExpRef
-                pure $ A.String $ T.pack val
-            Prim PBool -> do
-                b <- case map toLower val of
-                    "true"  -> pure True
-                    "false" -> pure False
-                    _       -> throwError $ "not Boolean: " ++ val
-                goRefs (LBool b) typeExpRef
-                pure $ A.Bool b
-            Prim PNull -> do
-                case map toLower val of
-                    "null" -> pure ()
-                    ""     -> pure ()
-                    _      -> throwError $ "not Null: " ++ val
-                pure $ A.Null
-            Prim PArray -> error "Array is not primitive"
-            Prim PObject -> error "Object is not primitive"
-
+    go TypeExpF {..} =
+      case typeExpName of
+        Prim PInt -> do
+          n <- ExceptT $ pure $ fmap fromInteger $ readEither val
+          goRefs (LNumber n) typeExpRef
+          pure $ A.Number n
+        Prim PNumber -> do
+          n <- ExceptT $ pure $ readEither val
+          goRefs (LNumber n) typeExpRef
+          pure $ A.Number n
+        Prim PString -> do
+          goRefs (LString val) typeExpRef
+          pure $ A.String $ T.pack val
+        Prim PBool -> do
+          b <- case map toLower val of
+            "true" -> pure True
+            "false" -> pure False
+            _ -> throwError $ "not Boolean: " ++ val
+          goRefs (LBool b) typeExpRef
+          pure $ A.Bool b
+        Prim PNull -> do
+          case map toLower val of
+            "null" -> pure ()
+            "" -> pure ()
+            _ -> throwError $ "not Null: " ++ val
+          pure $ A.Null
+        Prim PArray -> error "Array is not primitive"
+        Prim PObject -> error "Object is not primitive"
 
     goRefs :: Literal Desugar -> TypeRefine Desugar -> Except String ()
     goRefs lhs = mapM_ $ \(op, e) -> do
-        let rhs = evalExp M.empty (Root (Absolute "")) e
-        unless (interpret op [lhs, rhs] == LBool True) $
-            throwError $
-                "not match " ++ show (lhs, op, rhs)
+      let rhs = evalExp M.empty (Root (Absolute "")) e
+      unless (interpret op [lhs, rhs] == LBool True) $
+        throwError $
+          "not match " ++ show (lhs, op, rhs)
 
 evalExp :: M.Map ALocation Value -> ALocation -> Exp Desugar -> Literal Desugar
 evalExp env base (Exp e) = go e
-    where
+  where
     go (Literal l) = l
-    go (Var x)     =
-        case resolve base (toLocation x) of
-            Left loc -> toLiteral $ env M.! loc
-            Right i  -> LNumber (fromIntegral i)
+    go (Var x) =
+      case resolve base (toLocation x) of
+        Left loc -> toLiteral $ env M.! loc
+        Right i -> LNumber (fromIntegral i)
     go (App op args) = interpret op (map go args)
     toLocation = fmap $ \e ->
-        case evalExp env base e of
-            LNumber i -> floor i
-            v         -> error $ "index expected but found: " ++  show v
+      case evalExp env base e of
+        LNumber i -> floor i
+        v -> error $ "index expected but found: " ++ show v
+
 toLiteral :: Value -> Literal Desugar
-toLiteral (VBool b)   = LBool b
+toLiteral (VBool b) = LBool b
 toLiteral (VNumber n) = LNumber n
 toLiteral (VString s) = LString s
-toLiteral VNull       = LNull
-toLiteral VArray      = error "array is not a literal"
+toLiteral VNull = LNull
+toLiteral VArray = error "array is not a literal"
 toLiteral (VObject _) = error "object is not a literal"
-
-
