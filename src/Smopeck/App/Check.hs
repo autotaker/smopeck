50442740709366f7f33f74fc9a2f4a7bc1acb64f
autotaker <autotaker@gmail.com>
2022-11-03 17:26:55 +0900

    Verup ghc 9.2 (#3)
    * version up to ghc 9.2.4
    
    * upgrade versions in github actions
    
    * update Dockerfile
    
    * install libgmp10 from apt

--- src/Smopeck/App/Check.hs
+++ src/Smopeck/App/Check.hs
@@ -1,143 +1,160 @@
-{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeApplications  #-}
-module Smopeck.App.Check (
-    runApp
-)where
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
-import           Control.Lens
-import           Control.Monad.Except
-import           Control.Monad.Logger
-import qualified Data.Aeson               as A
+module Smopeck.App.Check
+  ( runApp,
+  )
+where
+
+import Control.Lens
+import Control.Monad.Except
+import Control.Monad.Logger
+import Data.Aeson (Value (Bool))
+import qualified Data.Aeson as A
 import qualified Data.Aeson.Encode.Pretty as A
-import           Data.Aeson.Lens
-import           Data.Bifunctor
-import qualified Data.ByteString          as BS
-import qualified Data.ByteString.Lazy     as LBS
-import           Data.CaseInsensitive     (CI)
-import qualified Data.CaseInsensitive     as CI
-import           Data.Either
-import qualified Data.Map                 as M
-import           Data.Maybe
-import           Data.Scientific
-import qualified Data.Text                as T
-import qualified Data.Text.Encoding       as T
-import qualified Data.Text.Lazy           as LT
-import qualified Data.Text.Lazy.Builder   as LT
-import           Network.HTTP.Client
-import           Network.HTTP.Types
-import           Paths_smopeck
-import           Smopeck.Config
-import qualified Smopeck.Mock.Constraint  as Constraint
-import           Smopeck.Spec.Preprocess
-import           Smopeck.Spec.Route
-import           Smopeck.Spec.TypeExp     (Route)
-import qualified Smopeck.Spec.Validator   as Validator
-import           System.IO
-import           TextShow
-
-import           Debug.Trace
+import qualified Data.Aeson.Key as K
+import Data.Aeson.Lens
+import Data.Bifunctor
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
+import Data.CaseInsensitive (CI)
+import qualified Data.CaseInsensitive as CI
+import Data.Either
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Scientific
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Builder as LT
+import Debug.Trace
+import Network.HTTP.Client
+import Network.HTTP.Types
+import Paths_smopeck
+import Smopeck.Config
+import qualified Smopeck.Mock.Constraint as Constraint
+import Smopeck.Spec.Preprocess
+import Smopeck.Spec.Route
+import Smopeck.Spec.TypeExp (Route)
+import qualified Smopeck.Spec.Validator as Validator
+import System.IO
+import TextShow
 
 prettyJSON :: A.Value -> T.Text
 prettyJSON val = LT.toStrict $ LT.toLazyText $ A.encodePrettyToTextBuilder val
 
-
-
 runApp :: CheckConfig -> LoggingT IO ()
-runApp CheckConfig{ targetURL = base, checkSmopeckFile = file} = do
-    $(logInfo) "Hello World Check App!"
-    manager <- liftIO $ newManager defaultManagerSettings
-    res <- runExceptT $ do
-        (typeEnv, endpoints) <- preprocess file
-        forM_ endpoints $ \DesugarEndpoint{..} -> do
-            $(logInfo) ("Endpoint: " <> showt (FromStringShow endpointMethod)
-                       <> " " <> showt endpointRoute)
-            param <- liftIO $ Constraint.mockJson typeEnv endpointParam
-            $(logDebug) $ "Param: " <>  prettyJSON param
-            let env = M.fromList [ ("parameter", param) ]
-            request <- liftIO $ Constraint.mockJsonWithEnv typeEnv env endpointRequest
-            $(logDebug) $ "Request: " <>  prettyJSON request
-            let req = buildRequest base endpointRoute endpointMethod param request
-            $(logDebug) $ "HttpRequest: " <> showt (FromStringShow req)
-            res <- liftIO $ httpLbs req manager
-            let response = fromResponse res
-                env' = M.fromList [ ("parameter", param)
-                                 , ("request", request)
-                                 , ("response", response)]
-            $(logDebug) $ "Response: " <> prettyJSON response
-            ExceptT $ pure $ Validator.validateJson typeEnv env' "response" endpointResponse
-    case res of
-        Left err -> $(logError) $ T.pack err
-        Right _  -> pure ()
+runApp CheckConfig {targetURL = base, checkSmopeckFile = file} = do
+  $(logInfo) "Hello World Check App!"
+  manager <- liftIO $ newManager defaultManagerSettings
+  res <- runExceptT $ do
+    (typeEnv, endpoints) <- preprocess file
+    forM_ endpoints $ \DesugarEndpoint {..} -> do
+      $(logInfo)
+        ( "Endpoint: "
+            <> showt (FromStringShow endpointMethod)
+            <> " "
+            <> showt endpointRoute
+        )
+      param <- liftIO $ Constraint.mockJson typeEnv endpointParam
+      $(logDebug) $ "Param: " <> prettyJSON param
+      let env = M.fromList [("parameter", param)]
+      request <- liftIO $ Constraint.mockJsonWithEnv typeEnv env endpointRequest
+      $(logDebug) $ "Request: " <> prettyJSON request
+      let req = buildRequest base endpointRoute endpointMethod param request
+      $(logDebug) $ "HttpRequest: " <> showt (FromStringShow req)
+      res <- liftIO $ httpLbs req manager
+      let response = fromResponse res
+          env' =
+            M.fromList
+              [ ("parameter", param),
+                ("request", request),
+                ("response", response)
+              ]
+      $(logDebug) $ "Response: " <> prettyJSON response
+      ExceptT $ pure $ Validator.validateJson typeEnv env' "response" endpointResponse
+  case res of
+    Left err -> $(logError) $ T.pack err
+    Right _ -> pure ()
 
 fromResponse :: Response LBS.ByteString -> A.Value
-fromResponse resp = A.object [
-        "header" A..= headerObj,
-        "body" A..= bodyObj
+fromResponse resp =
+  A.object
+    [ "header" A..= headerObj,
+      "body" A..= bodyObj
     ]
-    where
+  where
     headers = responseHeaders resp
     headerObj :: A.Value
     headerObj =
-        A.object $ do
-            (key, value) <- headers
-            let keyS = T.decodeUtf8 $ CI.original key
-                valueS = T.decodeUtf8 value
-            pure $ keyS A..= valueS
+      A.object $ do
+        (key, value) <- headers
+        let keyS = T.decodeUtf8 $ CI.original key
+            valueS = T.decodeUtf8 value
+        pure $ K.fromText keyS A..= valueS
     contentType =
-        headerObj ^? key "Content-Type"
-         & fromMaybe "text/plain"
+      headerObj ^? key "Content-Type"
+        & fromMaybe "text/plain"
     body = responseBody resp
     bodyObj :: A.Value
     bodyObj =
-        case contentType of
-            "application/json" ->
-                case A.eitherDecode' body of
-                    Left err  -> error err
-                    Right obj -> obj
-            _ -> A.String $ T.decodeUtf8 $ LBS.toStrict body
+      case contentType of
+        "application/json" ->
+          case A.eitherDecode' body of
+            Left err -> error err
+            Right obj -> obj
+        _ -> A.String $ T.decodeUtf8 $ LBS.toStrict body
 
 buildRequest :: String -> Route -> StdMethod -> A.Value -> A.Value -> Request
 buildRequest base route method paramObj reqObj =
-    parseRequest_ (show method ++ " " ++ base)
-     & setQueryString queryEnv
-     & (\req -> req { requestBody = body,
-                      requestHeaders = headers,
-                      path = path req <> pathRoute })
-    where
+  parseRequest_ (show method ++ " " ++ base)
+    & setQueryString queryEnv
+    & ( \req ->
+          req
+            { requestBody = body,
+              requestHeaders = headers,
+              path = path req <> pathRoute
+            }
+      )
+  where
     body = RequestBodyLBS $ A.encode bodyObj
     pathRoute = substRoute paramEnv route
-    paramEnv = M.fromList $ map (first T.unpack) pathParams
-    queryEnv = map (bimap T.encodeUtf8 (Just . T.encodeUtf8)) queryParams
+    paramEnv = M.fromList $ map (first K.toString) pathParams
+    queryEnv = map (bimap (T.encodeUtf8 . K.toText) (Just . T.encodeUtf8)) queryParams
     headers :: [(CI BS.ByteString, BS.ByteString)]
     headers =
-        reqObj ^.. key "header"
-                 . members
-                 . _String
-                 . withIndex
-                 . to (bimap (CI.mk . T.encodeUtf8) T.encodeUtf8)
+      reqObj
+        ^.. key "header"
+          . members
+          . _String
+          . withIndex
+          . to (bimap (CI.mk . T.encodeUtf8 . K.toText) T.encodeUtf8)
     bodyObj = fromMaybe A.Null $ reqObj ^? key "body"
     pathParams =
-        paramObj ^.. key "path"
-                   . members
-                   . _Primitive
-                   . to toText
-                   . withIndex
+      paramObj
+        ^.. key "path"
+          . members
+          . to toText
+          . _Just
+          . withIndex
     queryParams =
-        paramObj ^.. key "query"
-                   . members
-                   . _Primitive
-                   . to toText
-                   . withIndex
-    toText (StringPrim t)   = t
-    toText (NumberPrim v)   =
-        case floatingOrInteger v of
-            Left float -> T.pack $ show @Double float
-            Right int  -> T.pack $ show @Integer int
-    toText (BoolPrim True)  = "true"
-    toText (BoolPrim False) = "false"
-    toText NullPrim         = "null"
-
-
+      paramObj
+        ^.. key "query"
+          . members
+          . to toText
+          . _Just
+          . withIndex
+    toText :: A.Value -> Maybe T.Text
+    toText x
+      | isPrimitive x = Just $ T.decodeUtf8 $ BS.toStrict $ A.encode x
+      | otherwise = Nothing
+    isPrimitive :: A.Value -> Bool
+    isPrimitive (A.Object _) = False
+    isPrimitive (A.Array _) = False
+    isPrimitive A.Null = True
+    isPrimitive (A.Number _) = True
+    isPrimitive (A.Bool _) = True
+    isPrimitive (A.String _) = True
--- src/Smopeck/App/Mock.hs
+++ src/Smopeck/App/Mock.hs
@@ -1,147 +1,167 @@
-{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TemplateHaskell   #-}
-module Smopeck.App.Mock
-    ( runApp
-    , app
-    )
-where
-import           Control.Lens
-import           Control.Monad.Except
-import           Control.Monad.Logger
-import qualified Data.Aeson                 as A
-import           Data.Aeson.Lens
-import qualified Data.ByteString.Lazy.Char8 as LBS
-import qualified Data.CaseInsensitive       as CI
-import           Data.Function
-import qualified Data.HashMap.Strict        as HM
-import           Data.List                  (sortOn, unfoldr)
-import qualified Data.Map                   as M
-import           Data.Maybe
-import           Data.String                (fromString)
-import qualified Data.Text                  as T
-import qualified Data.Text.Encoding         as T
-import           Debug.Trace
-import           Network.HTTP.Types
-import           Network.Wai
-import qualified Network.Wai.Handler.Warp   as Warp
-import           Smopeck.Config
-import           Smopeck.Mock.Constraint
-import           Smopeck.Spec.Preprocess
-import           Smopeck.Spec.Route
-import           Smopeck.Spec.Syntax        hiding (Method, TypeEnv, TypeExp)
-import           Smopeck.Spec.TypeExp       (BindName (..), Route,
-                                             TypeCond (NoCond), TypeExpF (..),
-                                             TypeName (..), evalTypeEnv,
-                                             evalTypeExp)
-import           Smopeck.Spec.Validator
-import           System.IO
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 
+module Smopeck.App.Mock
+  ( runApp,
+    app,
+  )
+where
+
+import Control.Lens
+import Control.Monad.Except
+import Control.Monad.Logger
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Key as K
+import Data.Aeson.Lens
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.CaseInsensitive as CI
+import Data.Function
+import qualified Data.HashMap.Strict as HM
+import Data.List (sortOn, unfoldr)
+import qualified Data.Map as M
+import Data.Maybe
+import Data.String (fromString)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Debug.Trace
+import Network.HTTP.Types
+import Network.Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import Smopeck.Config
+import Smopeck.Mock.Constraint
+import Smopeck.Spec.Preprocess
+import Smopeck.Spec.Route
+import Smopeck.Spec.Syntax hiding (Method, TypeEnv, TypeExp)
+import Smopeck.Spec.TypeExp
+  ( BindName (..),
+    Route,
+    TypeCond (NoCond),
+    TypeExpF (..),
+    TypeName (..),
+    evalTypeEnv,
+    evalTypeExp,
+  )
+import Smopeck.Spec.Validator
+import System.IO
 
 runApp :: MockConfig -> LoggingT IO ()
-runApp MockConfig{ listenAddr = TcpConfig {..}, mockSmopeckFile = file } = do
-    let settings =
-            Warp.defaultSettings
-                & Warp.setPort tcpPort
-                & Warp.setHost (fromString tcpHost)
-    res <- runExceptT $ do
-        (typeEnv, endpoints) <- preprocess file
-        liftIO $ Warp.runSettings settings (app typeEnv endpoints)
-    case res of
-        Left err -> $(logError) (T.pack err)
-        Right () -> pure ()
+runApp MockConfig {listenAddr = TcpConfig {..}, mockSmopeckFile = file} = do
+  let settings =
+        Warp.defaultSettings
+          & Warp.setPort tcpPort
+          & Warp.setHost (fromString tcpHost)
+  res <- runExceptT $ do
+    (typeEnv, endpoints) <- preprocess file
+    liftIO $ Warp.runSettings settings (app typeEnv endpoints)
+  case res of
+    Left err -> $(logError) (T.pack err)
+    Right () -> pure ()
 
 app :: TypeEnv -> [DesugarEndpoint] -> Application
 app env defs req respond = go (sortOn (\def -> -length (endpointRoute def)) defs)
-    where
-        go [] = responseNotFound
-        go ((DesugarEndpoint route method tyParam tyReq tyRes):defs)
-            | Just param <- match route method tyParam = responseOk tyReq tyRes param
-            | otherwise = go defs
-        responseOk tyReq tyRes param = do
-            let reqHeaders = A.toJSON headers
-                headers = M.fromList [ (T.decodeUtf8 (CI.original key), T.decodeUtf8 val) | (key, val) <- requestHeaders req ]
-                request = A.object [ "header" A..= reqHeaders ]
-                valEnv = M.fromList [ ("request", request),
-                                      ("parameter", param) ]
-            reqBody <- case M.lookup "Content-Type" headers of
-                Just "application/json" ->
-                    fromJust . A.decodeStrict . LBS.toStrict <$> strictRequestBody req
-                _ -> pure A.Null
-            let request = A.object [ "header" A..= reqHeaders,
-                                     "body" A..= (reqBody :: A.Value) ]
-                valEnv = M.fromList [ ("request", request),
-                                      ("parameter", param) ]
-            case validateJson env valEnv "request" tyReq of
-                Left err -> responseInvalid $ err ++ "\ntyReq: " ++ show tyReq ++ "\nenv: " ++ show valEnv
-                Right () -> do
-                    v <- mockJsonWithEnv env valEnv tyRes
-                    let code = fromMaybe 200 $ v ^? key "status" . key "code" . _Integral
-                        status =
-                            maybe (toEnum code) (mkStatus code) $
-                                v ^? key "status"
-                                    . key "reason"
-                                    . _String
-                                    . to T.encodeUtf8
-                        headers =
-                                v ^.. key "header"
-                                    . members
-                                    . _String
-                                    . withIndex
-                                    . to (\(a,b) -> (fromString $ T.unpack a, T.encodeUtf8 b))
-                        body = fromMaybe "null" $ v ^? key "body" . to A.encode
-                    respond
-                        $ responseLBS
-                                status
-                                headers
-                                body
-        responseNotFound =
-            respond
-              $ responseLBS status404 [("Content-Type", "text/plain")] "Not Found"
-        responseInvalid err =
-            respond
-              $ responseLBS status400  [("Content-Type", "text/plain")] $ LBS.pack err
+  where
+    go [] = responseNotFound
+    go ((DesugarEndpoint route method tyParam tyReq tyRes) : defs)
+      | Just param <- match route method tyParam = responseOk tyReq tyRes param
+      | otherwise = go defs
+    responseOk tyReq tyRes param = do
+      let reqHeaders = A.toJSON headers
+          headers = M.fromList [(T.decodeUtf8 (CI.original key), T.decodeUtf8 val) | (key, val) <- requestHeaders req]
+          request = A.object ["header" A..= reqHeaders]
+          valEnv =
+            M.fromList
+              [ ("request", request),
+                ("parameter", param)
+              ]
+      reqBody <- case M.lookup "Content-Type" headers of
+        Just "application/json" ->
+          fromJust . A.decodeStrict . LBS.toStrict <$> strictRequestBody req
+        _ -> pure A.Null
+      let request =
+            A.object
+              [ "header" A..= reqHeaders,
+                "body" A..= (reqBody :: A.Value)
+              ]
+          valEnv =
+            M.fromList
+              [ ("request", request),
+                ("parameter", param)
+              ]
+      case validateJson env valEnv "request" tyReq of
+        Left err -> responseInvalid $ err ++ "\ntyReq: " ++ show tyReq ++ "\nenv: " ++ show valEnv
+        Right () -> do
+          v <- mockJsonWithEnv env valEnv tyRes
+          let code = fromMaybe 200 $ v ^? key "status" . key "code" . _Integral
+              status =
+                maybe (toEnum code) (mkStatus code) $
+                  v
+                    ^? key "status"
+                    . key "reason"
+                    . _String
+                    . to T.encodeUtf8
+              headers =
+                v
+                  ^.. key "header"
+                    . members
+                    . _String
+                    . withIndex
+                    . to (\(a, b) -> (fromString $ K.toString a, T.encodeUtf8 b))
+              body = fromMaybe "null" $ v ^? key "body" . to A.encode
+          respond $
+            responseLBS
+              status
+              headers
+              body
+    responseNotFound =
+      respond $
+        responseLBS status404 [("Content-Type", "text/plain")] "Not Found"
+    responseInvalid err =
+      respond $
+        responseLBS status400 [("Content-Type", "text/plain")] $
+          LBS.pack err
 
-        match route method tyParam = do
-                pathParam <- matchPath
-                guard matchMethod
-                queryParam <- matchQuery
-                pure $ A.object [ "query" A..= queryParam
-                                , "path" A..= pathParam ]
-            where
-            param = M.fromList [ (key , mvalue) | (key, mvalue) <- queryToQueryText $ queryString req ]
-            LElem TypeExpF{ typeExpExt = ext } = tyParam
-            LElem TypeExpF{ typeExpExt = queryExt } = evalTypeExp env $ ext  M.! FieldString "query"
-            LElem TypeExpF{ typeExpExt = pathExt } = evalTypeExp env $ ext  M.! FieldString "path"
-            matchMethod = parseMethod (requestMethod req) == Right method
-            matchQuery =
-                fmap A.object
-                  $ forM (M.assocs queryExt)
-                  $ \(FieldString field, ty) -> do
-                    let ty' = evalTypeExp env ty
-                    mv <- M.lookup (T.pack field) param
-                    v <- case mv of
-                        Nothing -> pure ""
-                        Just s  -> pure $ T.unpack s
-                    case runExcept (parseParam v ty') of
-                        Left _  -> Nothing
-                        Right v -> pure $ T.pack field A..= v
+    match route method tyParam = do
+      pathParam <- matchPath
+      guard matchMethod
+      queryParam <- matchQuery
+      pure $
+        A.object
+          [ "query" A..= queryParam,
+            "path" A..= pathParam
+          ]
+      where
+        param = M.fromList [(key, mvalue) | (key, mvalue) <- queryToQueryText $ queryString req]
+        LElem TypeExpF {typeExpExt = ext} = tyParam
+        LElem TypeExpF {typeExpExt = queryExt} = evalTypeExp env $ ext M.! FieldString "query"
+        LElem TypeExpF {typeExpExt = pathExt} = evalTypeExp env $ ext M.! FieldString "path"
+        matchMethod = parseMethod (requestMethod req) == Right method
+        matchQuery =
+          fmap A.object $
+            forM (M.assocs queryExt) $
+              \(FieldString field, ty) -> do
+                let ty' = evalTypeExp env ty
+                mv <- M.lookup (T.pack field) param
+                v <- case mv of
+                  Nothing -> pure ""
+                  Just s -> pure $ T.unpack s
+                case runExcept (parseParam v ty') of
+                  Left _ -> Nothing
+                  Right v -> pure $ K.fromString field A..= v
 
-            matchPath = do
-                let path = pathInfo req
-                    routeSeq = splitRoute route
-                    go (Elem s:rest) (txt:path) =
-                        guard (T.pack s == txt) >>
-                        go rest path
-                    go (Param p:rest) (txt:path) = do
-                        let ty = evalTypeExp env $ pathExt M.! FieldString p
-                        pair <- case runExcept (parseParam (T.unpack txt) ty) of
-                            Left _  -> Nothing
-                            Right v -> pure $ T.pack p A..= v
-                        (pair:) <$> go rest path
-                    go [] [] = pure []
-                    go _ _ = Nothing
-                A.object <$> go routeSeq path
-
-
+        matchPath = do
+          let path = pathInfo req
+              routeSeq = splitRoute route
+              go (Elem s : rest) (txt : path) =
+                guard (T.pack s == txt)
+                  >> go rest path
+              go (Param p : rest) (txt : path) = do
+                let ty = evalTypeExp env $ pathExt M.! FieldString p
+                pair <- case runExcept (parseParam (T.unpack txt) ty) of
+                  Left _ -> Nothing
+                  Right v -> pure $ K.fromString p A..= v
+                (pair :) <$> go rest path
+              go [] [] = pure []
+              go _ _ = Nothing
+          A.object <$> go routeSeq path
